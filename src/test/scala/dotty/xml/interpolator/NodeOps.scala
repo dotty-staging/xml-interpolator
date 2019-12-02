f3185019a16182a2fba20523ccfff6665436424a
Martin Odersky <odersky@gmail.com>
2019-12-02 17:40:27 +0100

    Drop old extension method syntax

--- src/test/scala/dotty/xml/interpolator/NodeOps.scala
+++ src/test/scala/dotty/xml/interpolator/NodeOps.scala
@@ -1,35 +1,29 @@
 package dotty.xml.interpolator
 
-given nodeOps: {
+given nodeOps: extension (self: scala.xml.Node) with
 
-  def (self: scala.xml.Node) ≈ (that: scala.xml.Node): Boolean =
+  def ≈ (that: scala.xml.Node): Boolean =
+    def hasSameScope(self: scala.xml.Node, that: scala.xml.Node): Boolean =
+      self.scope == that.scope
+      && {
+        val zipped = (self, that) match
+          case (g1: scala.xml.Group, g2: scala.xml.Group) => (g1.nodes, g2.nodes).zipped
+          case (n1, n2) => (n1.child, n2.child).zipped
+        zipped.forall(hasSameScope)
+      }
     self == that && hasSameScope(self, that)
 
-  def (self: scala.xml.Node) !≈ (that: scala.xml.Node): Boolean = !(self ≈ that)
+  def !≈ (that: scala.xml.Node): Boolean = !(self ≈ that)
 
-  private def hasSameScope(self: scala.xml.Node, that: scala.xml.Node): Boolean =
-    self.scope == that.scope && {
-      val zipped = (self, that) match {
-        case (g1: scala.xml.Group, g2: scala.xml.Group) => (g1.nodes, g2.nodes).zipped
-        case (n1, n2)                       => (n1.child, n2.child).zipped
-      }
-      zipped.forall(hasSameScope)
-    }
-}
 
-given nodeBufferOps: {
-  def (self: scala.xml.NodeBuffer) ≈ (that: scala.xml.NodeBuffer): Boolean = {
+given nodeBufferOps: extension (self: scala.xml.NodeBuffer) with
+  def ≈ (that: scala.xml.NodeBuffer): Boolean =
     val selfIt = self.iterator
     val thatIt = that.iterator
-
-    while (selfIt.hasNext && thatIt.hasNext) {
-      if (!(selfIt.next() ≈ thatIt.next())) {
+    while selfIt.hasNext && thatIt.hasNext do
+      if !(selfIt.next() ≈ thatIt.next()) then
         return false
-      }
-    }
-
     selfIt.isEmpty && thatIt.isEmpty
-  }
 
-  def (self: scala.xml.NodeBuffer) !≈ (that: scala.xml.NodeBuffer): Boolean = !(self ≈ that)
-}
+  def !≈ (that: scala.xml.NodeBuffer): Boolean = !(self ≈ that)
+
