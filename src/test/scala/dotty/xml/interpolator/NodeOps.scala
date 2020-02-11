bf2440fb65399c5c34e587e3d314d8d0cc4f98c3
Martin Odersky <odersky@gmail.com>
2020-02-11 23:13:11 +0100

    change with in CB (2)

--- src/test/scala/dotty/xml/interpolator/NodeOps.scala
+++ src/test/scala/dotty/xml/interpolator/NodeOps.scala
@@ -1,6 +1,6 @@
 package dotty.xml.interpolator
 
-extension nodeOps on (self: scala.xml.Node) with
+extension nodeOps on (self: scala.xml.Node):
 
   def ≈ (that: scala.xml.Node): Boolean =
     def hasSameScope(self: scala.xml.Node, that: scala.xml.Node): Boolean =
@@ -16,7 +16,7 @@ extension nodeOps on (self: scala.xml.Node) with
   def !≈ (that: scala.xml.Node): Boolean = !(self ≈ that)
 
 
-extension nodeBufferOps on (self: scala.xml.NodeBuffer) with
+extension nodeBufferOps on (self: scala.xml.NodeBuffer):
   def ≈ (that: scala.xml.NodeBuffer): Boolean =
     val selfIt = self.iterator
     val thatIt = that.iterator
