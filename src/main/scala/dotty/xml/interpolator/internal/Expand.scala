d8b2254fcc9a69091dd32ef5f0cdaf380c476473
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-11-12 15:08:29 +0100

    Use asReflectTree

--- src/main/scala/dotty/xml/interpolator/internal/Expand.scala
+++ src/main/scala/dotty/xml/interpolator/internal/Expand.scala
@@ -70,7 +70,7 @@ object Expand {
       }
       */
 
-      val term = value.unseal
+      val term = value.asReflectTree
       if (term.tpe <:< TypeRepr.of[String]) {
         val value = term.asExprOf[String]
         if (attribute.prefix.isEmpty) '{ new _root_.scala.xml.UnprefixedAttribute(${Expr(attribute.key)}, $value, $rest) }
